f10b976a67fe07ab70071c25b16c127d80f8bd4b
Ricardo Daniel <Ricperez0101@gmail.com>
2021-10-12 17:30:38 -0500

    Ciclos

---
+++ src/com/generation/Main.java
@@ -0,0 +1,33 @@
+package com.generation;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+//	Scanner sc = new Scanner(System.in);
+//	System.out.println("Ingresa un número");
+//	int numero = sc.nextInt();
+//
+//	for (int i = 0; i<=numero; i++){
+//		System.out.println(numero + "x" + i + "=" + (numero*i));
+//	}
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("ingresa un numero");
+//		int num = sc.nextInt();
+//		sc.close();
+//		int numi = 0;
+//		int numa = 0;
+//		int numb = 1;
+//		int numc = 0;
+//
+//		System.out.println("sucesion for : ");
+//		for(int i = 0; i < num; i++);
+//		if( numi < num){
+//			numc = numa + numb;
+//			numa = numb;
+//			numb = numc;
+//		} else {
+//			System.out.print(numa);
+//		}
+//
+//		System.out.println("");
+//}}
